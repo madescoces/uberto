014b963a6755f3dd76916895a987e40d5f18c000
madescoces <pablodaniel.foglia@emeal.nttdata.com>
2025-03-13 05:09:45 -0300

    Se finaliza de configurar el proyecto, agregando las dependencias y seteos de la aplicacion

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,16 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 plugins {
 	kotlin("jvm") version "1.9.25"
 	kotlin("plugin.spring") version "1.9.25"
+	kotlin("plugin.jpa") version "1.9.25"
 	id("org.springframework.boot") version "3.4.3"
 	id("io.spring.dependency-management") version "1.1.7"
+	jacoco
+}
+
+allOpen {
+	annotation("jakarta.persistence.Entity")
 }
 
 group = "ar.edu.unsam.phm"
@@ -18,20 +26,75 @@ repositories {
 	mavenCentral()
 }
 
+val mockkVersion = "1.13.9"
+val kotestVersion = "5.8.0"
+
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter")
+	developmentOnly("org.springframework.boot:spring-boot-devtools")
+
+	// básicos de cualquier proyecto Spring Boot
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.springframework.boot:spring-boot-starter-hateoas")
+	implementation("org.springframework.boot:spring-boot-starter-data-rest")
+	implementation("org.springframework.boot:spring-boot-starter-validation")
+	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+	implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
+
+	// conexión a la base de datos
+	runtimeOnly("org.postgresql:postgresql")
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation("org.springframework.boot:spring-boot-starter-data-mongodb")
+
+	// testing
+	testImplementation("com.h2database:h2:2.2.224")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
-	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+	testImplementation("org.mockito:mockito-core:3.+")
+	testImplementation("io.mockk:mockk:${mockkVersion}")
+	testImplementation("io.kotest:kotest-runner-junit5:$kotestVersion")
+	testImplementation("io.kotest:kotest-assertions-core:$kotestVersion")
+
+	// Swagger
+	implementation("org.springdoc:springdoc-openapi-starter-common:2.2.0")
+	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.2.0")
+
+	// Kotlin
+	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+	implementation(kotlin("stdlib"))
 }
 
-kotlin {
-	compilerOptions {
-		freeCompilerArgs.addAll("-Xjsr305=strict")
+tasks.withType<KotlinCompile> {
+	kotlinOptions {
+		freeCompilerArgs += "-Xjsr305=strict"
+		jvmTarget = "21"
 	}
 }
 
 tasks.withType<Test> {
 	useJUnitPlatform()
+	jvmArgs("-Xshare:off", "-XX:+EnableDynamicAgentLoading", "--add-opens", "java.base/java.time=ALL-UNNAMED")
+}
+
+tasks.test {
+	finalizedBy(tasks.jacocoTestReport)
+}
+
+jacoco {
+	toolVersion = "0.8.11"
+}
+
+tasks.jacocoTestReport {
+	dependsOn(tasks.test)
+	reports {
+		xml.required.set(true)
+		csv.required.set(true)
+		html.outputLocation.set(layout.buildDirectory.dir("jacocoHtml"))
+	}
+}
+
+tasks.register("runOnGitHub") {
+	dependsOn("jacocoTestReport")
+	group = "custom"
+	description = "$ ./gradlew runOnGitHub # runs on GitHub Action"
 }
